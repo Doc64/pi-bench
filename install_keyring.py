eab74a51a0b782e7c73485836de25a0c5e6b8975
Tom <thomas@chuckshipman.com>
2026-05-13 18:38:28 -0500

    Prune dev/diagnostic files, add .gitignore

--- install_keyring.py
+++
@@ -1,9 +0,0 @@
-import paramiko, sys
-c = paramiko.SSHClient()
-c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c.connect('192.168.200.163', username='tom', password='040102', timeout=10)
-stdin, stdout, stderr = c.exec_command('pip install keyring')
-print(stdout.read().decode(errors='replace'))
-print(stderr.read().decode(errors='replace'))
-print('exit:', stdout.channel.recv_exit_status())
-c.close()
